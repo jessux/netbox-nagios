1e7fa616dbb9d1897cb97db5e2f4e3cb792ab457
jessux <gabriel.kahlouche@gmail.com>
2020-12-08 12:35:13 +0100

    int

--- netbox_nagios/templatetags/nagios.py
+++ netbox_nagios/templatetags/nagios.py
@@ -4,12 +4,12 @@ register = template.Library()  # pylint: disable=invalid-name
 
 
 @register.filter(is_safe=True)
-def nagios_status_color(value: int):
+def nagios_status_color(value):
     local_colors = {0: "success", 1: "warning", 2: "danger", 3: "info"}
-    return local_colors.get(value, "default")
+    return local_colors.get(int(value), "default")
 
 
 @register.filter(is_safe=True)
-def nagios_status_string(value: int):
+def nagios_status_string(value):
     strings = {0: "OK", 1: "WARN", 2: "CRIT", 3: "UNKN"}
-    return strings.get(value, "NO DATA")
+    return strings.get(int(value), "NO DATA")
