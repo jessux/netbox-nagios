1c769e0827e316c5c733b0b05bd450cd1cd44f6b
jessux <gabriel.kahlouche@gmail.com>
2020-12-08 10:45:26 +0100

    test change live status

--- netbox_nagios/livestatus.py
+++ netbox_nagios/livestatus.py
@@ -1,36 +1,39 @@
 import json
-import socket
-
-TIMEOUT = 3  # seconds
-BUFFER_SIZE = 4096  # bytes
-
+import requests
 
 def hoststatus(hostname: str, livestatus_host: str, livestatus_port: int):
-    """Fetches livestatus from Nagios about hostname."""
-    query = (
-        "GET hosts\n"
-        + "Filter: host_name = %s\n" % hostname  #
-        + "OutputFormat: json\n"
-    )
+    # """Fetches livestatus from Nagios about hostname."""
+    # query = (
+    #     "GET hosts\n"
+    #     + "Filter: host_name = %s\n" % hostname  #
+    #     + "OutputFormat: json\n"
+    # )
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(TIMEOUT)
-    s.connect((livestatus_host, livestatus_port))
-    s.sendall(query.encode("utf-8"))
-    s.shutdown(socket.SHUT_WR)
+    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # s.settimeout(TIMEOUT)
+    # s.connect((livestatus_host, livestatus_port))
+    # s.sendall(query.encode("utf-8"))
+    # s.shutdown(socket.SHUT_WR)
 
     data = []
-    while True:
-        buf_data = s.recv(BUFFER_SIZE)
-        if not buf_data:
-            break
-        data.append(buf_data)
-    s.close()
+    # while True:
+    #     buf_data = s.recv(BUFFER_SIZE)
+    #     if not buf_data:
+    #         break
+    #     data.append(buf_data)
+    # s.close()
+    payload = {'username': 'admin','password': 'centreon'}
+    response = requests.request("POST", "http://digsflrp1k.dig.intra.groupama.fr/centreon/api/index.php?action=authenticate", data=payload)
+    if response.status_code == 200:
+        url = "http://digsflrp1k.dig.intra.groupama.fr/centreon/api/index.php?object=centreon_realtime_hosts&action=list&search="+hostname
+        payload = "{\r\n  \"action\": \"show\",\r\n  \"object\": \"host\"\r\n}"
+        headers = {
+            'Content-Type': 'application/json',
+            'centreon-auth-token': response.json()["authToken"],
+            'Content-Type': 'text/plain'
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
 
-    if not data:
-        return None
-    data = json.loads(b"".join(data))
+        json = response.json()
+        return {hostname,json.state}
 
-    if not data or len(data) <= 1:
-        return None
-    return dict(zip(data[0], data[1]))
